23cd98d7fd012e997e79bb3e4e5ae5c9d5405462
John Arundel <john@bitfieldconsulting.com>
2026-07-16 12:43:25 +0100

    memtest working

--- notes/memtest.asm
+++ notes/memtest.asm
@@ -1,22 +1,29 @@
-    ld  cd, 0xFFFF  ; start at top of memory
+    ld  cd, 0xFFFD  ; top of possible RAM
     ld  a, 0x02
-RAM_FILL:           
+
+RAM_FILL:
     ld  (cd), a     ; write 0x02 to each location
     dec cd
-    cmp cd, 0x1000  ; reached bottom of user memory?
+    cmp cd, 0x00FF  ; reached bottom of user memory?
     bne RAM_FILL    ; if not, keep going
 
 RAM_READ:
-    inc cd          ; update the pointer
-    cmp cd, 0x0000  ; gone past top?
-    beq DONE        ; if so, done
-    ld  a, (cd)
+    inc cd          ; pointer to next test address
+    cmp cd, 0xFFFE  ; past top?
+    beq DONE        ; if so, all memory is present and OK
+    ld  a, (cd)     ; get contents of address
+    cmp a, 0x02     ; should be 0x02
+    bne DONE        ; if not, RAM is faulty
     dec a           ; 0x02 goes to 0x01
-    beq DONE        ; but if zero then RAM is faulty
-    ld  (cd), a     ; store it back
-    ld  a, (cd)
-    dec (cd)        ; 0x01 goes to 0x00
-    beq RAM_READ    ; step to the next test unless it fails
+    ld  (cd), a     ; store 0x01 back in RAM
+    ld  a, (cd)     ; get contents again
+    cmp a, 0x01     ; should be 0x01
+    bne DONE        ; if not, RAM is faulty
+    dec a           ; 0x01 goes to 0x00
+    ld  (cd), a     ; store 0x00 back in RAM
+    ld  a, (cd)     ; get contents again
+    cmp a, 0x00     ; should be 0x00
+    beq RAM_READ    ; test next location
 
 DONE:
-    dec cd          ; cd points to the highest usable location
+    dec cd          ; cd points to the highest usable address
